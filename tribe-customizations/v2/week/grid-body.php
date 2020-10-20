61e669780013dd5577d0212bf85f004ad8942e30
András Guseo <andras@divi-magazine.com>
2020-10-20 20:01:09 +0200

    Adjusted opening and closing array brackets and...
    - removed some unneeded php tags
    - code formatting

--- tribe-customizations/v2/week/grid-body.php
+++ tribe-customizations/v2/week/grid-body.php
@@ -10,17 +10,16 @@
  *
  * See more documentation about our views templating system.
  *
- * @link http://m.tri.be/1aiy
+ * @link    http://m.tri.be/1aiy
  *
  * @version 5.0.0
  *
- * @var array $multiday_events An array of each day multi-day events and more event count, if any, in the shape
+ * @var array $multiday_events     An array of each day multi-day events and more event count, if any, in the shape
  *                                 `[ <Y-m-d> => [ 'events' => [ ...$multiday_events], 'more_events' => <int> ] ]`.
- * @var bool $has_multiday_events Boolean whether the week has multiday events or not.
- * @var array $events An array of each day non multi-day events, if any, in the shape `[ <Y-m-d> => [ ...$events ] ]`.
+ * @var bool  $has_multiday_events Boolean whether the week has multiday events or not.
+ * @var array $events              An array of each day non multi-day events, if any, in the shape `[ <Y-m-d> => [ ...$events ] ]`.
  */
-?>
-<?php
+
 /**
  * Get the settings
  */
@@ -36,20 +35,25 @@ $ext_options['show_grid']       = tribe_get_option( 'tribe_ext_limit_week_view_t
 		<div class="tribe-events-pro-week-grid__multiday-events-row-outer-wrapper">
 			<div class="tribe-events-pro-week-grid__multiday-events-row-wrapper">
 				<div
-						class="tribe-events-pro-week-grid__multiday-events-row"
-						data-js="tribe-events-pro-week-multiday-events-row"
-						role="row"
+					class="tribe-events-pro-week-grid__multiday-events-row"
+					data-js="tribe-events-pro-week-multiday-events-row"
+					role="row"
 				>
 
 					<?php $this->template( 'week/grid-body/multiday-events-row-header' ); ?>
 
-					<?php foreach ( $multiday_events as $day => list( $day_multiday_events, $more_events ) ) : ?>
-						<?php $this->template( 'week/grid-body/multiday-events-day',
-						                       [ 'day'         => $day,
-						                         'events'      => $day_multiday_events,
-						                         'more_events' => $more_events
-						                       ] ); ?>
-					<?php endforeach; ?>
+					<?php
+					foreach ( $multiday_events as $day => list( $day_multiday_events, $more_events ) ) :
+						$this->template(
+							'week/grid-body/multiday-events-day',
+							[
+								'day'         => $day,
+								'events'      => $day_multiday_events,
+								'more_events' => $more_events,
+							]
+						);
+					endforeach;
+					?>
 
 				</div>
 			</div>
@@ -67,13 +71,15 @@ $ext_options['show_grid']       = tribe_get_option( 'tribe_ext_limit_week_view_t
 					<?php
 					$count = 0;
 					foreach ( $events as $day => $day_events ) :
-						$this->template( 'week/grid-body/events-day',
-						                 [
-							                 'events'      => $day_events,
-							                 'ext_options' => $ext_options,
-							                 'count'       => $count
-						                 ] );
-						$count++;
+						$this->template(
+							'week/grid-body/events-day',
+							[
+								'events'      => $day_events,
+								'ext_options' => $ext_options,
+								'count'       => $count,
+							]
+						);
+						$count ++;
 					endforeach;
 					?>
 
