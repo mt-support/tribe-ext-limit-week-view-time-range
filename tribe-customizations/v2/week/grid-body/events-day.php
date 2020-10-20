21211ed3e95010ee64b7a48fedd77795fadd1da1
András Guseo <andras@divi-magazine.com>
2020-10-20 23:19:57 +0200

    Reformat conditions and add var info to header

--- tribe-customizations/v2/week/grid-body/events-day.php
+++ tribe-customizations/v2/week/grid-body/events-day.php
@@ -10,20 +10,30 @@
  *
  * See more documentation about our views templating system.
  *
- * @link http://m.tri.be/1aiy
+ * @link    http://m.tri.be/1aiy
  *
  * @version 5.0.0
  *
- * @var WP_Post[] $events The day events post objects.
+ * @var WP_Post[] $events      The day events post objects.
+ * @var array     $ext_options An array of the extension settings.
+ * @var int       $count       int to count which day of the week we are on.
  *
- * @see tribe_get_event() for the additional properties added to the event post object.
+ * @see     tribe_get_event() for the additional properties added to the event post object.
  */
 ?>
 
 <div class="tribe-events-pro-week-grid__events-day" role="gridcell">
 	<?php
-	if ( $count == 0 && $ext_options['show_grid'] ) {
-		for ( $i = 0; $i <= $ext_options['grid_end_time'] - $ext_options['grid_start_time']; $i++ ) {
+	/**
+	 * This will show a grid on the week view. Horizontal dashed lines across the 7 days at every hour.
+	 * Kind of the extension of the hour tick mark on the left side.
+	 * 'grid_start_time' and 'grid_end_time' are integers used in the CSS classes, not real date or time.
+	 */
+	if (
+		$count == 0
+		&& $ext_options['show_grid']
+	) {
+		for ( $i = 0; $i <= $ext_options['grid_end_time'] - $ext_options['grid_start_time']; $i ++ ) {
 			echo '<div class="tribe-events-pro-week-grid__events-day-gridlines" style="top: calc(' . $i . ' * 48px);"></div>';
 		}
 	}
