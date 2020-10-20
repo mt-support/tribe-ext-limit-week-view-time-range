6571b7bdd53c665350917f165b68ccede4898e6a
András Guseo <andras@divi-magazine.com>
2020-10-20 23:32:53 +0200

    Reformatting code
    and adding var info to header

--- tribe-customizations/v2/week/grid-body/events-day/event.php
+++ tribe-customizations/v2/week/grid-body/events-day/event.php
@@ -10,14 +10,14 @@
  *
  * See more documentation about our views templating system.
  *
- * @link http://m.tri.be/1aiy
+ * @link    http://m.tri.be/1aiy
  *
  * @version 5.0.0
  *
- * @var WP_Post $event The event post object with properties added by the `tribe_get_event` function.
+ * @var WP_Post $event       The event post object with properties added by the `tribe_get_event` function.
+ * @var array   $ext_options An array of the extension settings.
  *
- * @see tribe_get_event() For the format of the event object.
- *
+ * @see     tribe_get_event() For the format of the event object.
  */
 
 use Tribe__Date_Utils as Dates;
@@ -43,8 +43,7 @@ $data_js = [ 'tribe-events-pro-week-grid-event-link', 'tribe-events-tooltip' ];
 /**
  * Get start time in seconds
  */
-$start_time = Dates::time_between( $event->dates->start->format( 'Y-m-d 0:0:0' ),
-                                   $event->dates->start->format( Dates::DBDATETIMEFORMAT ) );
+$start_time = Dates::time_between( $event->dates->start->format( 'Y-m-d 0:0:0' ), $event->dates->start->format( Dates::DBDATETIMEFORMAT ) );
 
 /**
  * Get the extension settings
@@ -52,8 +51,14 @@ $start_time = Dates::time_between( $event->dates->start->format( 'Y-m-d 0:0:0' )
 $grid_start_time = $ext_options['grid_start_time'];
 $grid_end_time   = $ext_options['grid_end_time'];
 
+/**
+ * int containing if we have found the start time of an event based on the CSS class.
+ */
 $found_start_time = false;
 
+/**
+ * The pattern of the CSS class of the vertical offset.
+ */
 $pattern = '/(tribe-events-pro-week-grid__event--t-)/';
 
 foreach ( $classes as $key => $class ) {
@@ -64,8 +69,11 @@ foreach ( $classes as $key => $class ) {
 	}
 }
 
-// Hide the ones that would be in the 12-1 row. (They don't have the class.)
-if ( ! $found_start_time && $grid_start_time > 0 ) {
+// Hide the ones that would be in the 12 to 1 row. (They don't have the class.)
+if (
+	! $found_start_time
+	&& $grid_start_time > 0
+) {
 	$classes[] = 'tribe-common-a11y-visual-hide';
 } else {
 	// Set the new class
@@ -84,19 +92,24 @@ if ( ! $found_start_time && $grid_start_time > 0 ) {
 			$new_event_start_hour = (int) $time_split[0] - (int) $grid_start_time;
 
 			// Hide if...
-			if ( // Time is off the chart (negative start time)
-				( $new_event_start_hour <= 0 ) ||
+			if (
+				// Time is off the chart (negative start time)
+				$new_event_start_hour <= 0
 				// Time is in the first 15 minutes of the grid
-				( $grid_start_time === $new_event_start_hour && $time_split[1] <= 15 ) ||
+				|| (
+					$grid_start_time === $new_event_start_hour
+					&& $time_split[1] <= 15
+				)
 				// Time is before the grid start time
-				( $new_event_start_hour < $grid_start_time ) ||
-				// Time + 1 hour is after the grid end time (for long events at the end of the day
-				( $grid_end_time <= $time_split[0] + (int) $grid_start_time + 1 ) ) {
+				|| $new_event_start_hour < $grid_start_time
+				// Time + 1 hour is after the grid end time (for long events at the end of the day)
+				|| $grid_end_time <= $time_split[0] + (int) $grid_start_time + 1
+			) {
 				$classes[ $key ] = 'tribe-common-a11y-visual-hide';
 				break;
 			}
 
-			$time_split[0] = $new_event_start_hour;
+			$time_split[0]   = $new_event_start_hour;
 			$classes[ $key ] = 'tribe-events-pro-week-grid__event--t-' . implode( '-', $time_split );
 		}
 	}
@@ -104,16 +117,16 @@ if ( ! $found_start_time && $grid_start_time > 0 ) {
 ?>
 <article
 	<?php tribe_classes( $classes ) ?>
-		data-js="tribe-events-pro-week-grid-event"
-		data-start-time="<?php echo esc_attr( $start_time ); ?>"
-		data-event-id="<?php echo esc_attr( $event->ID ); ?>"
+	data-js="tribe-events-pro-week-grid-event"
+	data-start-time="<?php echo esc_attr( $start_time ); ?>"
+	data-event-id="<?php echo esc_attr( $event->ID ); ?>"
 >
 	<a
-			href="<?php echo esc_url( $event->permalink ); ?>"
-			class="tribe-events-pro-week-grid__event-link"
-			data-js="<?php echo esc_attr( implode( ' ', $data_js ) ); ?>"
-			data-tooltip-content="#tribe-events-tooltip-content-<?php echo esc_attr( $event->ID ); ?>"
-			aria-describedby="tribe-events-tooltip-content-<?php echo esc_attr( $event->ID ); ?>"
+		href="<?php echo esc_url( $event->permalink ); ?>"
+		class="tribe-events-pro-week-grid__event-link"
+		data-js="<?php echo esc_attr( implode( ' ', $data_js ) ); ?>"
+		data-tooltip-content="#tribe-events-tooltip-content-<?php echo esc_attr( $event->ID ); ?>"
+		aria-describedby="tribe-events-tooltip-content-<?php echo esc_attr( $event->ID ); ?>"
 	>
 		<div class="tribe-events-pro-week-grid__event-link-inner">
 
